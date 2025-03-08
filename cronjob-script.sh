1a6fa468e36e4aaa888664f340d4e3f3c8117793
anish-lakkapragada <anish.lakkapragada@gmail.com>
2025-03-08 00:57:25 -0800

    nightly build

--- cronjob-script.sh
+++ cronjob-script.sh
@@ -1,40 +1,45 @@
+PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+export PATH
+
+cd ~/Documents/git-repos/anish-lakkapragada.github.io
+
 # fall 2024 term 
-rsync -av --delete ~/Documents/classes/old-classes/math-226/psets notes/math-226
-rsync -av --delete ~/Documents/classes/old-classes/math-226/sols notes/math-226
+/usr/bin/rsync -av --delete ~/Documents/classes/old-classes/math-226/psets notes/math-226
+/usr/bin/rsync -av --delete ~/Documents/classes/old-classes/math-226/sols notes/math-226
 
-sh rename-files.sh notes/math-226/psets
-sh rename-files.sh notes/math-226/sols
+/bin/sh rename-files.sh notes/math-226/psets
+/bin/sh rename-files.sh notes/math-226/sols
 
-rsync -av --delete ~/Documents/classes/old-classes/stats-241/psets notes/stats-241
-rsync -av --delete ~/Documents/classes/old-classes/stats-241/sols notes/stats-241
+/usr/bin/rsync -av --delete ~/Documents/classes/old-classes/stats-241/psets notes/stats-241
+/usr/bin/rsync -av --delete ~/Documents/classes/old-classes/stats-241/sols notes/stats-241
 
-sh rename-files.sh notes/stats-241/psets
-sh rename-files.sh notes/stats-241/sols
+/bin/sh rename-files.sh notes/stats-241/psets
+/bin/sh rename-files.sh notes/stats-241/sols
 
 # spring 2025 term 
-rsync -av --delete ~/Documents/math-255/psets notes/math-255
-rsync -av --delete ~/Documents/math-255/sols notes/math-255
+/usr/bin/rsync -av --delete ~/Documents/math-255/psets notes/math-255
+/usr/bin/rsync -av --delete ~/Documents/math-255/sols notes/math-255
 
-sh rename-files.sh notes/math-255/psets
-sh rename-files.sh notes/math-255/sols
+/bin/sh rename-files.sh notes/math-255/psets
+/bin/sh rename-files.sh notes/math-255/sols
 
-rsync -av --delete ~/Documents/math-244/psets notes/math-244
-rsync -av --delete ~/Documents/math-244/sols notes/math-244
+/usr/bin/rsync -av --delete ~/Documents/math-244/psets notes/math-244
+/usr/bin/rsync -av --delete ~/Documents/math-244/sols notes/math-244
 
-sh rename-files.sh notes/math-244/psets
-sh rename-files.sh notes/math-244/sols
+/bin/sh rename-files.sh notes/math-244/psets
+/bin/sh rename-files.sh notes/math-244/sols
 
-rsync -av --delete ~/Documents/stats-242/psets notes/stats-242
-rsync -av --delete ~/Documents/stats-242/sols notes/stats-242
+/usr/bin/rsync -av --delete ~/Documents/stats-242/psets notes/stats-242
+/usr/bin/rsync -av --delete ~/Documents/stats-242/sols notes/stats-242
 
-sh rename-files.sh notes/stats-242/psets
-sh rename-files.sh notes/stats-242/sols
+/bin/sh rename-files.sh notes/stats-242/psets
+/bin/sh rename-files.sh notes/stats-242/sols
 
-sh build.sh
+/bin/sh build.sh
 
-git add . 
-git commit -m "nightly build" 
-git push
+/usr/bin/git add . 
+/usr/bin/git commit -m "nightly build" 
+/usr/bin/git push
 
-rm -rf notes/math-*
-rm -rf notes/stats-*
+/bin/rm -rf notes/math-*
+/bin/rm -rf notes/stats-*
